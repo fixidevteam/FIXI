237c098601627b404a92030b140be371588c84e8
shadow007 <nourdinehoumame0@gmail.com>
2024-11-10 17:25:59 +0100

    limit 5 elements in dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,14 +18,6 @@
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-[#F9FAFB]">
         @include('layouts.n')
-        <!-- Page Heading -->
-        {{-- @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-    </div>
-    </header>
-    @endif --}}
 
     <!-- Page Content -->
     <main>
