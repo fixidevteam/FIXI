58a12427d04d202a3f00e7d19e855570bbfd56bc
shadow007 <nourdinehoumame0@gmail.com>
2024-12-10 15:05:15 +0100

    edit content

--- resources/views/userOperations/index.blade.php
+++ resources/views/userOperations/index.blade.php
@@ -32,7 +32,7 @@
                             <a
                                 href="{{ route('operation.index') }}"
                                 class="inline-flex items-center text-sm font-medium text-gray-700   ">
-                                Mes opération
+                                Mes opérations
                             </a>
                         </div>
                     </li>
@@ -45,17 +45,6 @@
             <div class=" px-5 py-3 text-gray-700 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="flex justify-between items-center my-6">
                     <h2 class="text-2xl font-bold leading-9 tracking-tight text-gray-900">Liste des opérations</h2>
-                    {{-- <div>
-                        <a href="{{ route('voiture.create') }}">
-                            <x-primary-button class="hidden md:block">Ajouter voiture</x-primary-button>
-                            <x-primary-button class="sm:hidden">
-                                <svg class="w-5 h-5 text-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12.75 9C12.75 8.58579 12.4142 8.25 12 8.25C11.5858 8.25 11.25 8.58579 11.25 9L11.25 11.25H9C8.58579 11.25 8.25 11.5858 8.25 12C8.25 12.4142 8.58579 12.75 9 12.75H11.25V15C11.25 15.4142 11.5858 15.75 12 15.75C12.4142 15.75 12.75 15.4142 12.75 15L12.75 12.75H15C15.4142 12.75 15.75 12.4142 15.75 12C15.75 11.5858 15.4142 11.25 15 11.25H12.75V9Z" fill="currentColor" />
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z" fill="currentColor" />
-                                </svg>
-                            </x-primary-button>
-                        </a>
-                    </div> --}}
                 </div>
                 {{-- table --}}
                 <div class="my-5">
