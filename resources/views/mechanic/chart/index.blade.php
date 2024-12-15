32ad9b10d66d5f2bd479c328e94a141e4dfd60eb
shadow007 <nourdinehoumame0@gmail.com>
2024-12-15 13:54:16 +0100

    add chart of the year to analitic page on mechanic space

--- resources/views/mechanic/chart/index.blade.php
+++ resources/views/mechanic/chart/index.blade.php
@@ -45,7 +45,31 @@
             <div class=" px-5 py-3 text-gray-700 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <h2 class="mt-10 text-2xl font-bold leading-9 tracking-tight text-gray-900">Analytique</h2>
                 <div class="container">
-                    <h2 class="text-sm md:text-base">Suivi des opérations et des clients (3 derniers mois)</h2>
+                    <div class="my-4">
+                        <h2 class="text-sm md:text-base mb-4">Suivi des opérations (tout les années)</h2>
+                        <!-- THE CHART  -->
+                        <form method="GET" action="{{ route('mechanic.chart') }}" class="mb-4 w-full flex flex-col sm:flex-row sm:items-end items-end justify-between  gap-4">
+                            <!-- Dropdown Container -->
+                            <div class="flex flex-col w-full  ">
+                            <label for="year" class="text-sm font-medium text-gray-700 mb-1 sm:mb-0">Sélectionnez l'année :</label>
+                            <select name="year" id="year" class="block mt-1 w-full rounded-md border-0 py-1.5 text-sm text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                @foreach($years as $year)
+                                <option value="{{ $year }}" {{ $year == $selectedYear ? 'selected' : '' }}>
+                                {{ $year }}
+                                </option>
+                                @endforeach
+                            </select>
+                            </div>
+                
+                            <!-- Submit Button -->
+                            <x-primary-button class="">
+                            {{ __('Filter') }}
+                            </x-primary-button>
+                        </form>
+                        <canvas id="operations" height="200" class="w-full bg-white"></canvas>
+                        <!-- END OF CHART -->
+                    </div>
+                    <h2 class="text-sm md:text-base mb-4">Suivi des opérations et des clients (3 derniers mois)</h2>
                     <div class="my-4">
                         <canvas id="operationsChart"></canvas>
                         <canvas id="clientsChart" class="mt-4"></canvas>
@@ -60,6 +84,42 @@
             @include('layouts.footer')
         </div>
     </div>
+    <script>
+        // Data passed from the controller
+        const labels = @json($labels); // Month names
+        const values = @json($values); // Operation counts
+    
+        const ctx = document.getElementById('operations').getContext('2d');
+        new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: labels, // Labels for the x-axis
+            datasets: [{
+              label: 'Nombre des operations',
+              data: values, // Data for the y-axis
+              backgroundColor: 'rgb(220, 38, 28)',
+              borderColor: 'rgb(220, 38, 28)',
+              borderWidth: 1,
+              tension: 0.4,
+    
+            }]
+          },
+          options: {
+            responsive: true,
+            scales: {
+              y: {
+                beginAtZero: true,
+                ticks: {
+                  stepSize: 1,
+                  callback: function(value) {
+                    return Number.isInteger(value) ? value : '';
+                  }
+                }
+              }
+            }
+          }
+        });
+      </script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             fetch("{{ route('mechanic.analytics.data') }}")
