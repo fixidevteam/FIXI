05cb080253db02e4556d8d9fb57e059bbf094d65
shadow007 <nourdinehoumame0@gmail.com>
2024-12-24 12:12:40 +0100

    add fixi-plus & fixi-pro logos for spaces user & mechanic

--- resources/views/layouts/n.blade.php
+++ resources/views/layouts/n.blade.php
@@ -25,7 +25,7 @@
                     {{-- logo --}}
                     <div class="shrink-0 flex items-center">
                         <a href="{{ route('dashboard') }}">
-                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                            <x-application-plus-logo class="block h-9 w-auto fill-current text-gray-800" />
                         </a>
                     </div>
                     {{-- logo end --}}
