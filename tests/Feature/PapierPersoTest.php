a78f6a5ec7139919b86f70f6dd1073a15d23eb39
Andromofid <ymofid18@gmail.com>
2025-01-01 18:42:37 +0100

    add tests: PapierPersoTest,ConseilsEntretienTest

--- tests/Feature/PapierPersoTest.php
+++ tests/Feature/PapierPersoTest.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\UserPapier;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Mockery\Undefined;
 use Tests\TestCase;
 
 class PapierPersoTest extends TestCase
@@ -15,41 +16,48 @@ class PapierPersoTest extends TestCase
      * A basic feature test example.
      * 
      */
-    use RefreshDatabase;
+    // use RefreshDatabase;
     public function test_papiers_perso(): void
     {
         $user = User::factory()->create(['status' => 1, 'ville' => 'marrakech']);
-        $res = $this->actingAs($user)->get('fixi-plus/paiperPersonnel');
-        $res->assertOk();
+        $res = $this->actingAs($user)->get(route('paiperPersonnel.index'));
+        $res->assertViewIs('userPaiperPersonnel.index');
+    }
+    public function test_undefind_type()
+    {
+        $user = User::factory()->create(['status' => 1, 'ville' => 'marrakech']);
+        $res = $this->actingAs($user)->post(
+            route('paiperPersonnel.store'),
+            [
+                'type' => 'undefind',
+                'date_debut' => now(),
+                'date_fin' => now()
+            ]
+        );
+        $res->assertSessionHasErrors('type');
     }
     public function test_ajouter_pp_get_notification(): void
     {
-        $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
         // Create a user and log in
         $user = User::factory()->create(['status' => 1, 'ville' => 'marrakech']);
-        $this->actingAs($user);
-
         // Create a valid type in the database
         type_papierp::create([
             'type' => 'Passeport'
         ]);
-
-        // Make the POST request
-        $response = $this->post(
-            'fixi-plus/paiperPersonnel',
+        $response = $this->withMiddleware("CheckUserDocuments")->actingAs($user)->post(
+            route('paiperPersonnel.store'),
             [
                 'type' => 'Passeport',
                 'date_debut' => now(),
                 'date_fin' => now(), // Ensure valid date range
             ]
         );
+        // Make the POST request
         $response->assertStatus(302);
         // Assert that the response is successful
         $this->assertDatabaseHas('user_papiers', [
             'user_id' => $user->id,
         ]);
-
+        $this->assertDatabaseCount('notifications', 1);
     }
-
-
 }
